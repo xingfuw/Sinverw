2fab1b38cff74905369936da8d7a6430a0887f8d
Seereo <xingfu.wang@gmail.com>
2025-07-30 23:52:39 -0700

    update code files

---
+++ cs50p-xw/calculator.py
@@ -0,0 +1,12 @@
+def main():
+    x = int(input("What's x? "))
+    print("x squred is", square(x))
+    
+
+def square(n):
+    return n * n
+
+
+
+
+main()
---
+++ cs50p-xw/hello.py
@@ -0,0 +1,16 @@
+def main():
+    hello()
+    name = input("What is your name? ")
+    hello(name)
+    
+
+
+
+
+
+def hello(to = "world"):
+    print("hello,", to)
+
+
+main()# hello.py
+# A program that greets the user by name.
