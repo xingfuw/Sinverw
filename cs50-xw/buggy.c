793664d3b525c722edc1b2ffcd08efa881e81bd6
Seereo <xingfu.wang@gmail.com>
2025-07-30 23:54:15 -0700

    update -2

--- cs50-xw/buggy.c
+++ cs50-xw/buggy.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+void print_column(int height);
+
 int main(void) 
 {
-    for (int i = 0; i <= 3; i++) 
+    int h;
+    printf("Height: ");
+    scanf("%d", &h);
+    print_column(h);
+}
+
+void print_column(int height) 
+{
+    for (int i = 0; i < height; i++) 
     {
         printf("#\n");
     }
-}    
+}
+// The function print_column prints a column of '#' characters based on the given height.
